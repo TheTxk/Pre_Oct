3be41833194bb58b7407974c6d8c3949f2abcb64
TheTxk <116103149+TheTxk@users.noreply.github.com>
2024-10-20 15:58:26 +1100

    Update script.js

--- script.js
+++ script.js
@@ -46,9 +46,9 @@ function displayCat() {
     var imageContainer = document.getElementById('image-container');
     // Create a new Image element for the cat
     var catImage = new Image();
-    // Set the source (file path) for the cat image
-    catImage.src = 'shopping-cat.gif'; // Assuming the cat image is named "cat.gif"
-    // Set alternative text for the image (for accessibility)
+    
+    catImage.src = 'shopping-cat.gif'; 
+    
     catImage.alt = 'Cat';
     // When the cat image is fully loaded, add it to the image container
     catImage.onload = function() {
